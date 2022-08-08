72fedc398d9abde4d58dd7fd235dd94847215925
Leila Moraes <leila.violino@gmail.com>
2022-08-08 12:06:16 -0300

    Resolução dos requisitos 7 e 8 e mudanças nas funções generateColors e boardSize

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
 window.onload = function() {
   const colorBlack = document.querySelector('#color-black');
   colorBlack.classList.add('selected');
-  //const colorPalette = document.querySelector('#color-palette'); 
-  //colorPalette.addEventListener('click', send);
+  const colorPalette = document.querySelectorAll('.color'); 
+  for (let color of colorPalette) {
+  color.addEventListener('click', send);
+  }
   
-  //function send(event) {
-    //let colorSelected = event.target.classList.replace('color', 'selected');
-
-  //}
-
-  
+  function send(event) {
+    for (let elements of colorPalette) {
+      if (elements.classList.contains('selected')) {
+        elements.classList.remove('selected');
+      }
+    }
+    event.target.classList.add('selected');
+  }
+    
   function generateColors() {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255); 
-    const rgb = 'rgb' + '(' + r + ', ' + g + ', ' + b + ')';
+    const rgb = `rgb(${r},${g}, ${b})`;
     return rgb;
   }
-
+  
   function associateColor() {
     const color = document.querySelectorAll('.color');
-      for (let i = 1; i < color.length; i += 1) {
+    for (let i = 1; i < color.length; i += 1) {
       color[i].style.backgroundColor = generateColors();
-      }
+    }
     return color;
   }
   console.log(associateColor());
 
+  /*function createBtn() {
+    const pai = document.body;
+    const btn = document.createElement('button');
+    btn.id = 'clear-board';
+    btn.innerText = 'Limpar';
+    pai.appendChild(btn);
+    return btn;
+  }
+  console.log(createBtn());*/
+  
   function createBoard() {
     const pai = document.body;
     const board = document.createElement('div');
@@ -34,8 +49,10 @@ window.onload = function() {
     pai.appendChild(board);
     return board;
   }
-
+  createBoard();
+  
   function boardSize(size) {
+    const board = document.querySelector('#pixel-board');
     for (let i = 1; i <= size; i += 1) {
       const boardWidth = document.createElement('div');
       for (let ind = 1; ind <= size; ind += 1) {
@@ -43,11 +60,11 @@ window.onload = function() {
         boardHeight.className = 'pixel';
         boardWidth.appendChild(boardHeight);
       }
-      createBoard().appendChild(boardWidth);
+      board.appendChild(boardWidth);
     }
   }
   boardSize(5);
-
+  
   function styleChanges() {
     const pixels = document.querySelectorAll('.pixel');
     for (const elements of pixels) {
@@ -58,5 +75,26 @@ window.onload = function() {
     return pixels;
   }
   console.log(styleChanges());
+  
+  let boardPixels = document.querySelectorAll('.pixel');
+  for (let pixels of boardPixels) {
+    pixels.addEventListener('click', coloredBoard);
+  }
 
+  function coloredBoard(event) {
+    const pixel = document.querySelector('.selected');
+    const bgPixelSelected = getComputedStyle(pixel).backgroundColor;
+    event.target.style.backgroundColor = bgPixelSelected;
+  }
+
+  /*function btnClear(event) {
+    const btn = document.getElementById('clear-board');
+    console.log(btn);
+    btn.addEventListener('click', function(){
+      let pixelWhite = document.querySelectorAll('.pixel');
+      for (let elements of pixelWhite){
+        elements.target.style.backgroundColor = 'white' 
+      }
+    })
+  }*/
 }
